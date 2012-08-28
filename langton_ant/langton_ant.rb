7602923aea79884a2a893011772e881dfe56be84
Ryland Herrick <ryland@bendyworks.com>
2012-08-27 23:25:50 -0500

    Animate progression of board

--- langton_ant/langton_ant.rb
+++ langton_ant/langton_ant.rb
@@ -2,10 +2,18 @@ require 'rubygems'
 require 'bundler/setup'
 
 Bundler.require
-
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-10.times do |i|
-  steps = (i+1) * 10_000
+include Magick
+
+20.times do |i|
+  steps = (i+1) * 5_000
   Board.new(:steps => steps, :filename => "#{i}.png").run!
 end
+
+slides = Dir['output/*.png'].sort_by do |f|
+  f.gsub('output/', '').split('.')[0].to_i
+end
+animation = ImageList.new(*slides)
+animation.delay = 10
+animation.write("output/output.gif")
