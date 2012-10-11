8c08f19e3c5f5a82f067af882b1625184fda2c95
Ryland Herrick <ryland@bendyworks.com>
2012-10-11 15:14:24 -0500

    Add ability to specify open/close chars for #generate

--- balancing/balancer.rb
+++ balancing/balancer.rb
@@ -1,37 +1,45 @@
 require 'rspec'
 describe 'Balancer' do
-  let(:subject) { Balancer.generate(count) }
+  let(:subject) { Balancer.generate(count, options) }
+  let(:options) { {} }
   let(:count) { 1 }
 
-  it 'works for 1' do
-    subject.should =~ %w|[]|
+  context 'one pair' do
+    example { should =~ %w|[]| }
   end
 
   context 'two pairs' do
     let(:count) { 2 }
-    example { subject.should =~ %w|[][] [[]]| }
-
+    example { should =~ %w|[][] [[]]| }
   end
 
   context 'three pairs' do
     let(:count) { 3 }
-    example { subject.should =~ %w|[][][] [[]][] [][[]] [[][]] [[[]]]| }
+    example { should =~ %w|[][][] [[]][] [][[]] [[][]] [[[]]]| }
+  end
+
+  context 'optional open/close' do
+    let(:options) { { open: '(', close: ')' } }
+    example { should =~ %w|()| }
   end
 end
 
 class Balancer
-  def self.generate(count = 1)
-    pairs(count)
+  DEFAULT_OPTIONS = { open: '[', close: ']' } unless const_defined? :DEFAULT_OPTIONS
+
+  def self.generate(count = 1, options = {})
+    options = DEFAULT_OPTIONS.merge(options)
+    pairs(count, options)
   end
 
   private
-  def self.pairs(count)
+  def self.pairs(count, options)
     return [''] if count == 0
     result = []
     for size in 0...count
-      for pair in pairs(size)
-        for new_pair in pairs(count-size-1)
-          result << "[#{pair}]#{new_pair}"
+      for pair in pairs(size, options)
+        for new_pair in pairs(count-size-1, options)
+          result << options[:open] + pair + options[:close] + new_pair
         end
       end
     end
